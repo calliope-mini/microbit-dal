c419efd55d578ff8ca2e4e45f52ab054633a3ec7
Hugo <hugo.woehrl@calliope.cc>
2023-08-03 17:38:29 +0200

    change pins according to calliope mini classic

--- inc/drivers/MicroBitPin.h
+++ inc/drivers/MicroBitPin.h
@@ -38,26 +38,34 @@ DEALINGS IN THE SOFTWARE.
 #define IO_STATUS_EVENT_ON_EDGE             0x20        // Pin will generate events on pin change
 #define IO_STATUS_EVENT_PULSE_ON_EDGE       0x40        // Pin will generate events on pin change
 
-//#defines for each edge connector pin
-#define MICROBIT_PIN_P0                     P0_3        //P0 is the left most pad (ANALOG/DIGITAL) used to be P0_3 on green board
-#define MICROBIT_PIN_P1                     P0_2        //P1 is the middle pad (ANALOG/DIGITAL)
-#define MICROBIT_PIN_P2                     P0_1        //P2 is the right most pad (ANALOG/DIGITAL) used to be P0_1 on green board
-#define MICROBIT_PIN_P3                     P0_4        //COL1 (ANALOG/DIGITAL)
-#define MICROBIT_PIN_P4                     P0_5        //COL2 (ANALOG/DIGITAL)
-#define MICROBIT_PIN_P5                     P0_17       //BTN_A
-#define MICROBIT_PIN_P6                     P0_12       //COL9
-#define MICROBIT_PIN_P7                     P0_11       //COL8
-#define MICROBIT_PIN_P8                     P0_18       //PIN 18
-#define MICROBIT_PIN_P9                     P0_10       //COL7
-#define MICROBIT_PIN_P10                    P0_6        //COL3 (ANALOG/DIGITAL)
-#define MICROBIT_PIN_P11                    P0_26       //BTN_B
-#define MICROBIT_PIN_P12                    P0_20       //PIN 20
-#define MICROBIT_PIN_P13                    P0_23       //SCK
-#define MICROBIT_PIN_P14                    P0_22       //MISO
-#define MICROBIT_PIN_P15                    P0_21       //MOSI
-#define MICROBIT_PIN_P16                    P0_16       //PIN 16
-#define MICROBIT_PIN_P19                    P0_0        //SCL
-#define MICROBIT_PIN_P20                    P0_30       //SDA
+//#defines for each edge connector pin, changed for Calliope mini V1 & V2
+#define MICROBIT_PIN_P0                     P0_0        //P0 
+#define MICROBIT_PIN_P1                     P0_1        //P1 analog
+#define MICROBIT_PIN_P2                     P0_2        //P2 analog
+#define MICROBIT_PIN_P3                     P0_3        //P3 
+#define MICROBIT_PIN_P4                     P0_4        //LEDCOL1	analog
+#define MICROBIT_PIN_P5                     P0_5        //LEDCOL2	analog
+#define MICROBIT_PIN_P6                     P0_6        //LEDCOL3	analog
+#define MICROBIT_PIN_P7                     P0_7        //LEDCOL4	
+#define MICROBIT_PIN_P8                     P0_8        //LEDCOL5	
+#define MICROBIT_PIN_P9                     P0_9        //LEDCOL6	
+#define MICROBIT_PIN_P10                    P0_10       //LEDCOL7	
+#define MICROBIT_PIN_P11                    P0_11       //LEDCOL8	
+#define MICROBIT_PIN_P12                    P0_12       //LEDCOL9	
+#define MICROBIT_PIN_P13                    P0_13       //LEDROW1	
+#define MICROBIT_PIN_P14                    P0_14       //LEDROW2	
+#define MICROBIT_PIN_P15                    P0_15       //LEDROW3	
+#define MICROBIT_PIN_P16                    P0_26       //RX analog
+#define MICROBIT_PIN_P17                    P0_27       //TX analog
+#define MICROBIT_PIN_P19                    P0_19       //SCL
+#define MICROBIT_PIN_P20                    P0_20       //SDA
+#define MICROBIT_PIN_BTNA                   P0_17       //BTN_A
+#define MICROBIT_PIN_BTNB                   P0_16       //BTN_B
+#define MICROBIT_PIN_RGB                    P0_18       //RGB 
+#define MICROBIT_PIN_MIC                    P0_3        //MIC analog
+#define MICROBIT_PIN_M_SLEEP                P0_28       //Motor Sleep
+#define MICROBIT_PIN_M_IN1                  P0_29       //Motor IN1
+#define MICROBIT_PIN_M_IN2                  P0_30       //Motor IN2
 
 #define MICROBIT_PIN_MAX_OUTPUT             1023
 
